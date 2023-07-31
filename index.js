fcdbb106b04a08d08c4d996cc4ab7f67e7e745c0
nuraansa <nuraansa@gmail.com>
2023-07-31 16:11:34 +0200

    exercise2

---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require('./person')
+
+const person1 = new Person('Nuraan', 'Samodien', 'nuraansa1@gmail.com')
+const person2 = new Person('Ishmaeel', 'Davids', 'ishmadavids@gmail.com')
+const person3 = new Person('Joel', 'Mukanya', 'joelmukanya@lifechoices.org')
+
+console.log(person1)
+console.log(person2)
+console.log(person3)
---
+++ person.js
@@ -0,0 +1,7 @@
+function Persons(firstName, lastName, email) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+}
+
+module.exports = Persons
